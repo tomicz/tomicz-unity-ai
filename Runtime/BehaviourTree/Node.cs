420b49c6bef64fcc85110a12439c6c01169b5af2
Darko <tomiczdarko@gmail.com>
2024-04-21 17:05:25 +0200

    Addd, remove and set data recuresvely for every node

--- Runtime/BehaviourTree/Node.cs
+++ Runtime/BehaviourTree/Node.cs
@@ -23,5 +23,61 @@ namespace Tomicz.AI.BehaviourTree
         }
 
         public abstract NodeStatus Tick();
+
+        public void SetData(string key, object value)
+        {
+            _data[key] = value;
+        }
+
+        public object GetData(string key)
+        {
+            object value = null;
+
+            if(_data.TryGetValue(key, out value))
+            {
+                return value;
+            }
+
+            Node node = ParentNode;
+
+            while(node != null)
+            {
+                value = node.GetData(key);
+
+                if(value != null)
+                {
+                    return value;
+                }
+
+                node = node.ParentNode;
+            }
+
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+            if(_data.ContainsKey(key)) 
+            {
+                    _data.Remove(key);
+                    return true;
+            }
+
+            Node node = ParentNode;
+
+            while(node != null)
+            {
+                bool isCleared = node.ClearData(key);
+
+                if(isCleared)
+                {
+                    return true;
+                }
+
+                node = node.ParentNode;
+            }
+
+            return false;
+        }
     }
 }
